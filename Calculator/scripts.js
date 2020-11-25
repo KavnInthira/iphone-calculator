963f75d42089677a7f8b38a6dad498d1502e94c1
Kavin Inthirakot <kainthirakot@gmail.com>
2020-11-25 06:58:27 -0500

    add text file to mark task and add more function to buttons numerical buttons

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,4 +1,44 @@
-function change_text() {
+
+
+function clear() {}
+
+function change_toZero() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "0";
+}
+function change_toOne() {
     var x = document.getElementById("screen");
     x.innerHTML = "1";
+  }
+  function change_toTwo() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "2";
+}
+function change_toThree() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "3";
+  }
+  function change_toFour() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "4";
+}
+function change_toFive() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "5";
+  }
+  function change_toSix() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "6";
+}
+function change_toSeven() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "7";
+  }
+  function change_toEight() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "8";
+}
+function change_toNine() {
+    var x = document.getElementById("screen");
+    x.innerHTML = "9";
   }
